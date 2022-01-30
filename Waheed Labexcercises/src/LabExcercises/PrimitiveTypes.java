11cf0b21a14715007a3893f3c7725445661645fc
Waheed <Waheed@Waheed>
2022-01-30 20:12:09 +0530

    commit

---
+++ Waheed Labexcercises/src/LabExcercises/PrimitiveTypes.java
@@ -0,0 +1,33 @@
+package LabExcercises;
+
+public class PrimitiveTypes {
+	public static void main(String[] args) {
+		int a =2147483647;
+		int b =-2147483648;
+		byte c = 127;
+		byte d = -128;
+		short e = 32767;
+		short f =-32768;
+		long g = 1234567890;
+		long h = -1234567890;
+		float i = 32.14f;
+		double j = 34.888888;
+		boolean k = true;
+		boolean l = false;
+		System.out.println(a);
+	    System.out.println(b);
+	    System.out.println(c);
+	    System.out.println(d);
+	    System.out.println(e);
+	    System.out.println(f);
+	    System.out.println(g);
+	    System.out.println(h);
+	    System.out.println(i);
+	    System.out.println(j);
+	    System.out.println(k);
+	    System.out.println(l);
+	}
+	
+			
+
+}
